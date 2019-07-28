456327e1f282967270316d233189d3c0cda930af
Bhanu Singh <bhanu@krenovate.com>
2019-07-28 22:26:57 +0530

    Init Commit

---
+++ public/js/script.js
@@ -0,0 +1,31 @@
+
+
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#p1')
+const messageTwo = document.querySelector('#p2')
+
+messageOne.textContent = 'FormContent'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location =search.value 
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+    messageOne.textContent = data.error
+            }
+            else{
+    messageOne.textContent = data.location
+    messageTwo.textContent = data.forecast
+
+    console.log(data.location)
+    console.log(data.forecast)
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/2d3ddc64b4810b23c3d63a02010d66ab/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
